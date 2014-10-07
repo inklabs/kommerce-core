bdf98528e37c209cf09a9cee1345d9d358e9f1a4
Jamie Isaacs <pdt256@gmail.com>
2014-10-07 00:05:39 -0700

    Adding cart coupons.

--- src/Entity/CartTotal.php
+++ src/Entity/CartTotal.php
@@ -5,11 +5,11 @@ class CartTotal
 {
 	use Accessors;
 
-	public $orig_subtotal = 0;
-	public $subtotal = 0;
-	public $discount = 0;
+	public $orig_subtotal = 0;     // Used to calculate total savings
+	public $subtotal = 0;          // Subtotal after catalog promotions
+	public $discount = 0;          // Coupons and cart price rules
 	public $shipping = 0;
-	public $shipping_discount = 0;
+	public $shipping_discount = 0; // Coupons and cart price rules (separate from $discount)
 	public $tax = 0;
 	public $total = 0;
 	public $savings = 0;
