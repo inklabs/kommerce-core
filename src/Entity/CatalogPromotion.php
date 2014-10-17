172d59a00fed5736d568fd9b181101955d692565
Jamie Isaacs <pdt256@gmail.com>
2014-10-16 20:07:39 -0700

    Cleaning up xml mapper.

--- src/Entity/CatalogPromotion.php
+++ src/Entity/CatalogPromotion.php
@@ -1,51 +1,22 @@
 <?php
 namespace inklabs\kommerce\Entity;
 
-/**
- * @Entity(repositoryClass="Doctrine\ORM\EntityRepository")
- * @Table(name="catalog_promotion")
- **/
 class CatalogPromotion extends Promotion
 {
-    /** @Id @Column(type="integer") @GeneratedValue **/
     protected $id;
-
-    /** @Column(type="string") **/
     protected $name;
-
-    /** @Column(type="string", name="discount_type") **/
     protected $discountType;
-
-    /** @Column(type="integer", name="discount_value") **/
     protected $value;
-
-    /** @Column(type="boolean", name="free_shipping") **/
     protected $flagFreeShipping;
-
-    /** @Column(type="integer") **/
     protected $redemptions;
-
-    /** @Column(type="integer", name="max_redemptions") **/
     protected $maxRedemptions;
-
-    /** @Column(type="date") **/
     protected $start;
-
-    /** @Column(type="date") **/
     protected $end;
-
-    /** @Column(type="integer") **/
     protected $created;
-
-    /** @Column(type="integer") **/
     protected $updated;
+    protected $tag;
 
     protected $code;
-
-    /**
-     * @ManyToOne(targetEntity="Tag", fetch="EAGER")
-    **/
-    protected $tag;
 
     public function setCode($code)
     {
