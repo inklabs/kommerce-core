f3931dc391c615d1fe40825f4358ad085afc953a
Jamie Isaacs <pdt256@gmail.com>
2014-10-17 11:40:50 -0700

    Getting entities to 100%

--- tests/Entity/Accessor/UpdatedTest.php
+++ tests/Entity/Accessor/UpdatedTest.php
@@ -3,16 +3,11 @@ namespace inklabs\kommerce;
 
 use inklabs\kommerce\Entity\Accessor\Updated;
 
-class UpdatedMock
-{
-    use Updated;
-}
-
 class UpdatedTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $this->updatedMock = new UpdatedMock;
+        $this->updatedMock = new Entity\Product;
         $this->updatedMock->setUpdated(new \DateTime('now', new \DateTimeZone('UTC')));
     }
 
