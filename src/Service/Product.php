aa176c5fd63cb4112997aac2074c3e8cbf9e7b39
Jamie Isaacs <pdt256@gmail.com>
2014-10-19 01:02:46 -0700

    Working with many-to-many in service layer.

--- src/Service/Product.php
+++ src/Service/Product.php
@@ -19,4 +19,61 @@ class Product extends EntityManager
 
         return $product;
     }
+
+    public function getRelatedProducts($product, $limit = 12)
+    {
+        $pricingService = new Pricing($this->entityManager);
+        $pricing = $pricingService->getPricing();
+
+        $qb = $this->entityManager->createQueryBuilder();
+
+        $query = $qb->select('product')
+            ->from('inklabs\kommerce\Entity\Product', 'product')
+            ->where('product.id <> :productId');
+
+        $parameters = [
+            'productId' => $product->getId()
+        ];
+
+        $tags = $product->getTags();
+        if (! empty($tags)) {
+            $parameters['tagIds'] = [];
+            foreach ($tags as $tag) {
+                $parameters['tagIds'][] = $tag->getId();
+            }
+
+            $query = $query
+                ->innerJoin('product.tags', 'tag')
+                ->andWhere('tag.id IN (:tagIds)');
+        }
+
+        $query = $query
+            ->setMaxResults($limit)
+            ->setParameters($parameters);
+
+        // TODO:
+        // ->where('product.id', 'NOT IN', $product_ids)
+        // ->where('product.visible', '=', 1)
+        // ->where_open()
+        //     ->where('product.require_inventory', '=', 1)
+        //     ->where('product.quantity', '>', 0)
+        //     ->or_where_open()
+        //         ->where('product.require_inventory', '=', 0)
+        //     ->or_where_close()
+        // ->where_close()
+        // ->where('product.active', '=', 1)
+        // ->group_by('product.id')
+        // ->order_by($order_by)
+        // ->limit($limit)
+        // ->find_all()
+
+        $relatedProducts = $query->getQuery()->getResult();
+
+        foreach ($relatedProducts as $relatedProduct) {
+            $price = $pricing->getPrice($relatedProduct, 1);
+            $relatedProduct->setPriceObj($price);
+        }
+
+        return $relatedProducts;
+    }
 }
