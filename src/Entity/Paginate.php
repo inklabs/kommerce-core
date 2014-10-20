c07a84052f3bd0f987fee59ccb28533c0ae7c23f
Jamie Isaacs <pdt256@gmail.com>
2014-10-20 05:22:17 -0700

    Cleaning up pagination.

--- src/Entity/Paginate.php
+++ src/Entity/Paginate.php
@@ -5,15 +5,20 @@ use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class Paginate
 {
-    public $maxResults;
-    public $page;
-    public $total;
-    public $loadTotal = true;
+    private $maxResults;
+    private $page;
+    private $total;
+    private $loadTotal = true;
 
     public function __construct($maxResults = 10, $page = 1)
     {
         $this->maxResults = $maxResults;
         $this->page = $page;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
     }
 
     public function paginate(\Doctrine\ORM\QueryBuilder $query)
