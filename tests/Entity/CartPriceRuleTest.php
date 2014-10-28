c063d167934d35a1685007e8e3b1dbae7178a8b8
Jamie Isaacs <jamie@trinetcloud.com>
2014-10-27 18:30:14 -0700

    Cleaning up cart price rules.

--- tests/Entity/CartPriceRuleTest.php
+++ tests/Entity/CartPriceRuleTest.php
@@ -29,4 +29,41 @@ class CartPriceRuleTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(2, count($this->cartPriceRule->getItems()));
         $this->assertEquals(1, count($this->cartPriceRule->getDiscounts()));
     }
+
+    public function testIsCartItemsValid()
+    {
+        $productShirt = new Product;
+        $productShirt->setSku('TS-NAVY-LG');
+        $productShirt->setName('Navy T-shirt (large)');
+        $productShirt->setPrice(1200);
+
+        $productPoster = new Product;
+        $productPoster->setSku('PST-CKN');
+        $productPoster->setName('Citizen Kane (1941) Poster');
+        $productPoster->setPrice(500);
+
+        $cartPriceRule = new CartPriceRule;
+        $cartPriceRule->setName('Buy a Shirt get a FREE poster');
+        $cartPriceRule->setStart(new \DateTime('2014-01-01', new \DateTimeZone('UTC')));
+        $cartPriceRule->setEnd(new \DateTime('2014-12-31', new \DateTimeZone('UTC')));
+        $cartPriceRule->addItem(new CartPriceRuleItem($productShirt, 1));
+        $cartPriceRule->addItem(new CartPriceRuleItem($productPoster, 1));
+        $cartPriceRule->addDiscount(new CartPriceRuleDiscount($productPoster, 1));
+
+        $reflector = new \ReflectionClass('inklabs\kommerce\Entity\Cart');
+        $property = $reflector->getProperty('items');
+        $property->setAccessible(true);
+
+        $cart = new Cart;
+        $cartItems = $property->getValue($cart);
+        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
+
+        $cart->addItem($productShirt, 1);
+        $cartItems = $property->getValue($cart);
+        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
+
+        $cart->addItem($productPoster, 1);
+        $cartItems = $property->getValue($cart);
+        $this->assertTrue($cartPriceRule->isCartItemsValid($cartItems));
+    }
 }
