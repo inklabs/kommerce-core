80477712ca4b6c179af6b4c013969ccbbe8a7c6c
Jamie Isaacs <pdt256@gmail.com>
2014-11-22 14:33:17 -0800

    Code cleanup.

--- tests/Service/TaxRateTest.php
+++ tests/Service/TaxRateTest.php
@@ -12,22 +12,21 @@ class TaxRateTest extends Helper\DoctrineTestCase
     public function setUp()
     {
         $this->taxRates = [
-            $this->getDummyTaxRate('CA', null, null, null, 7.5, true),
-            $this->getDummyTaxRate(null, 92606, null, null, 8, true),
-            $this->getDummyTaxRate(null, 92612, null, null, 8, true),
-            $this->getDummyTaxRate(null, null, 92602, 92604, 8, true),
+            $this->getTaxRate('CA', null, null, null, 7.5, true),
+            $this->getTaxRate(null, 92606, null, null, 8, true),
+            $this->getTaxRate(null, 92612, null, null, 8, true),
+            $this->getTaxRate(null, null, 92602, 92604, 8, true),
         ];
 
-        $this->entityManager->persist($this->taxRates[0]);
-        $this->entityManager->persist($this->taxRates[1]);
-        $this->entityManager->persist($this->taxRates[2]);
-        $this->entityManager->persist($this->taxRates[3]);
+        foreach ($this->taxRates as $taxRate) {
+            $this->entityManager->persist($taxRate);
+        }
         $this->entityManager->flush();
 
         $this->taxRateService = new TaxRate($this->entityManager);
     }
 
-    private function getDummyTaxRate($state, $zip5, $zip5From, $zip5To, $rate, $applyToShipping)
+    private function getTaxRate($state, $zip5, $zip5From, $zip5To, $rate, $applyToShipping)
     {
         $taxRate = new Entity\TaxRate;
         $taxRate->setState($state);
