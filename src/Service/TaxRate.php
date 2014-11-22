d1bc1f3f7fb1c50a614d7dc6e8821e221028a7f8
Jamie Isaacs <pdt256@gmail.com>
2014-11-22 15:56:30 -0800

    Adding tax rate searching.

--- src/Service/TaxRate.php
+++ src/Service/TaxRate.php
@@ -1,6 +1,7 @@
 <?php
 namespace inklabs\kommerce\Service;
 
+use inklabs\kommerce\Entity as Entity;
 use inklabs\kommerce\Lib as Lib;
 use Doctrine as Doctrine;
 
@@ -14,5 +15,36 @@ class TaxRate extends Lib\EntityManager
     public function findAll()
     {
         return $this->entityManager->getRepository('inklabs\kommerce\Entity\TaxRate')->findAll();
+    }
+
+    public function findByZip5AndState($zip5 = null, $state = null)
+    {
+        $qb = $this->createQueryBuilder();
+
+        $query = $qb->select('tax_rate')
+            ->from('inklabs\kommerce\Entity\TaxRate', 'tax_rate');
+
+        if ($state !== null) {
+            $query = $query
+                ->where('tax_rate.state = :state')
+                ->setParameter('state', $state);
+        }
+
+        if ($zip5 !== null) {
+            $query = $query
+                ->orWhere('tax_rate.zip5 = :zip5')
+                ->orWhere('tax_rate.zip5From <= :zip5 AND tax_rate.zip5To >= :zip5')
+                ->orderBy('tax_rate.id')
+                ->setParameter('zip5', $zip5);
+        }
+
+        $taxRates = $query->findAll();
+
+        $viewTaxRates = [];
+        foreach ($taxRates as $taxRate) {
+            $viewTaxRates[] = Entity\View\TaxRate::factory($taxRate);
+        }
+
+        return $viewTaxRates;
     }
 }
--- tests/Service/TaxRateTest.php
+++ tests/Service/TaxRateTest.php
@@ -41,7 +41,34 @@ class TaxRateTest extends Helper\DoctrineTestCase
     public function testFindAll()
     {
         $taxRates = $this->taxRateService->findAll();
-
         $this->assertEquals($this->taxRates, $taxRates);
+    }
+
+    public function testFindByZip5AndStateEmpty()
+    {
+        $taxRates = $this->taxRateService->findByZip5AndState();
+        $this->assertEquals(4, count($taxRates));
+        $this->assertEquals(7.5, $taxRates[0]->rate);
+    }
+
+    public function testFindByZip5AndStateWithZip5()
+    {
+        $taxRates = $this->taxRateService->findByZip5AndState('92606');
+        $this->assertEquals(1, count($taxRates));
+        $this->assertEquals(8, $taxRates[0]->rate);
+    }
+
+    public function testFindByZip5AndStateWithState()
+    {
+        $taxRates = $this->taxRateService->findByZip5AndState(null, 'CA');
+        $this->assertEquals(1, count($taxRates));
+        $this->assertEquals(7.5, $taxRates[0]->rate);
+    }
+
+    public function testFindByZip5AndStateWithZip5AndState()
+    {
+        $taxRates = $this->taxRateService->findByZip5AndState('92606', 'CA');
+        $this->assertEquals(2, count($taxRates));
+        $this->assertEquals(7.5, $taxRates[0]->rate);
     }
 }
