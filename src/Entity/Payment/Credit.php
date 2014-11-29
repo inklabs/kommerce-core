baf0c67a132b402339a33de61eca6bc0a493b3b0
Jamie Isaacs <pdt256@gmail.com>
2014-11-28 23:47:54 -0800

    Removing storing credit card.

--- src/Entity/Payment/Credit.php
+++ src/Entity/Payment/Credit.php
@@ -7,14 +7,12 @@ use inklabs\kommerce\Lib\PaymentGateway\ChargeResponse;
 
 class Credit extends Payment
 {
-    protected $creditCard;
     protected $charge;
 
     public function __construct(ChargeRequest $chargeRequest, Gateway $gateway)
     {
         $this->setCreated();
         $this->amount = $chargeRequest->getAmount();
-        $this->creditCard = $chargeRequest->getCreditCard();
         $this->setCharge($gateway->getCharge($chargeRequest));
     }
 
