2e30a5ea8cf76236ec1ab1db0559108954a7a0fe
Jamie Isaacs <pdt256@gmail.com>
2014-11-28 17:00:51 -0800

    Cleaning up cart price rules.

--- tests/Entity/CartPriceRuleTest.php
+++ tests/Entity/CartPriceRuleTest.php
@@ -5,33 +5,6 @@ use inklabs\kommerce\Service as Service;
 
 class CartPriceRuleTest extends \PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
-        $this->productShirt = new Product;
-        $this->productShirt->setSku('TS-NAVY-LG');
-        $this->productShirt->setName('Navy T-shirt (large)');
-        $this->productShirt->setUnitPrice(1200);
-
-        $this->productPoster = new Product;
-        $this->productPoster->setSku('PST-CKN');
-        $this->productPoster->setName('Citizen Kane (1941) Poster');
-        $this->productPoster->setUnitPrice(500);
-
-        $this->cartPriceRule = new CartPriceRule;
-        $this->cartPriceRule->setName('Buy a Shirt get a FREE poster');
-        $this->cartPriceRule->setStart(new \DateTime('2014-01-01', new \DateTimeZone('UTC')));
-        $this->cartPriceRule->setEnd(new \DateTime('2014-12-31', new \DateTimeZone('UTC')));
-        $this->cartPriceRule->addItem(new CartPriceRuleItem($this->productShirt, 1));
-        $this->cartPriceRule->addItem(new CartPriceRuleItem($this->productPoster, 1));
-        $this->cartPriceRule->addDiscount(new CartPriceRuleDiscount($this->productPoster, 1));
-    }
-
-    public function testGetters()
-    {
-        $this->assertEquals(2, count($this->cartPriceRule->getItems()));
-        $this->assertEquals(1, count($this->cartPriceRule->getDiscounts()));
-    }
-
     public function testIsCartItemsValid()
     {
         $productShirt = new Product;
