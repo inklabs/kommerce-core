3ca5f6ce4da88541cfef39538baf3336393e8a68
Jamie Isaacs <pdt256@gmail.com>
2014-11-29 13:16:40 -0800

    Adding flattened product info and discount names.

--- src/Entity/OrderItem.php
+++ src/Entity/OrderItem.php
@@ -18,23 +18,45 @@ class OrderItem
     protected $catalogPromotions;
     protected $productQuantityDiscount;
 
+    /* Flattened Columns */
+    protected $productSku;
+    protected $productName;
+    protected $discountNames;
+
     public function __construct(CartItem $cartItem, Pricing $pricing)
     {
         $this->catalogPromotions = new ArrayCollection;
-        $this->product = $cartItem->getProduct();
         $this->quantity = $cartItem->getQuantity();
+
+        $this->setProduct($cartItem->getProduct());
         $this->setPrice($cartItem->getPrice($pricing));
+    }
+
+    private function setProduct(Product $product)
+    {
+        $this->product = $product;
+
+        $this->productSku = $product->getSku();
+        $this->productName = $product->getName();
     }
 
     private function setPrice(Price $price)
     {
         $this->price = $price;
 
+        $discountNames = [];
         foreach ($price->getCatalogPromotions() as $catalogPromotion) {
             $this->catalogPromotions[] = $catalogPromotion;
+            $discountNames[] = $catalogPromotion->getName();
         }
 
         $this->productQuantityDiscount = $price->getProductQuantityDiscount();
+
+        if ($this->productQuantityDiscount !== null) {
+            $discountNames[] = $this->productQuantityDiscount->getName();
+        }
+
+        $this->discountNames = implode(', ', $discountNames);
     }
 
     public function getId()
