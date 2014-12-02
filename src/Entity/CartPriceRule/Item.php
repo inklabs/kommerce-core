6b5610e7dbed4634c94e3cd60dfe19f585ff20c3
Jamie Isaacs <pdt256@gmail.com>
2014-12-01 23:48:07 -0800

    Adding cart price rule tests.

--- src/Entity/CartPriceRule/Item.php
+++ src/Entity/CartPriceRule/Item.php
@@ -12,25 +12,4 @@ abstract class Item
     protected $quantity;
 
     abstract public function matches(Entity\CartItem $cartItem);
-
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function setQuantity($quantity)
-    {
-        $this->quantity = $quantity;
-        $this->setUpdated();
-    }
-
-    public function getQuantity()
-    {
-        return $this->quantity;
-    }
 }
--- src/Entity/CartPriceRule/Product.php
+++ src/Entity/CartPriceRule/Product.php
@@ -30,9 +30,4 @@ class Product extends Item
     {
         return $this->product;
     }
-
-    public function setProduct(Product $product)
-    {
-        $this->product = $product;
-    }
 }
