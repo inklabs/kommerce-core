25376ed01f2e13cb54d5480cdc6a95bd149f9fda
Jamie Isaacs <pdt256@gmail.com>
2014-12-02 13:42:58 -0800

    Reorganize for 100% for CartPriceRule.

--- tests/Entity/CartPriceRuleTest.php
+++ tests/Entity/CartPriceRuleTest.php
@@ -5,101 +5,81 @@ use inklabs\kommerce\Service as Service;
 
 class CartPriceRuleTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCartPriceRuleProduct()
+    public function testCreate()
     {
-        $productShirt = new Product;
-        $productShirt->setSku('TS-NAVY-LG');
-        $productShirt->setName('Navy T-shirt (large)');
-        $productShirt->setUnitPrice(1200);
+        $cartPriceRule = new CartPriceRule;
+        $this->assertEquals(0, count($cartPriceRule->getItems()));
+        $this->assertEquals(0, count($cartPriceRule->getDiscounts()));
+    }
 
-        $productPoster = new Product;
-        $productPoster->setSku('PST-CKN');
-        $productPoster->setName('Citizen Kane (1941) Poster');
-        $productPoster->setUnitPrice(500);
+    public function testAdders()
+    {
+        $cartPriceRule = new CartPriceRule;
+        $cartPriceRule->addItem(new CartPriceRule\Product(new Product, 1));
+        $cartPriceRule->addDiscount(new CartPriceRuleDiscount);
+        $this->assertEquals(1, count($cartPriceRule->getItems()));
+        $this->assertEquals(1, count($cartPriceRule->getDiscounts()));
+    }
+
+    public function testIsCartItemsValid()
+    {
+        $product = new Product;
+        $product->setid(1);
 
         $cartPriceRule = new CartPriceRule;
-        $cartPriceRule->setName('Buy two Shirts (TS-NAVY-LG) get a FREE poster (PST-CKN)');
-        $cartPriceRule->setStart(new \DateTime('2014-01-01', new \DateTimeZone('UTC')));
-        $cartPriceRule->setEnd(new \DateTime('2014-12-31', new \DateTimeZone('UTC')));
-        $cartPriceRule->addItem(new CartPriceRule\Product($productShirt, 2));
-        $cartPriceRule->addItem(new CartPriceRule\Product($productPoster, 1));
-        $cartPriceRule->addDiscount(new CartPriceRuleDiscount($productPoster, 1));
+        $cartPriceRule->addItem(new CartPriceRule\Product($product, 1));
 
-        $pricing = new Service\Pricing;
+        $cartItems = [new CartItem($product, 1)];
 
-        $cart = new Cart;
-        $cartItems = $cart->getItems();
-        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
-
-        $cart = new Cart($pricing);
-        $cart->addItem($productShirt, 1);
-        $cartItems = $cart->getItems();
-        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
-
-        $cart = new Cart($pricing);
-        $cart->addItem($productShirt, 1);
-        $cart->addItem($productPoster, 1);
-        $cartItems = $cart->getItems();
-        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
-
-        $cart = new Cart($pricing);
-        $cart->addItem($productShirt, 2);
-        $cart->addItem($productPoster, 1);
-        $cartItems = $cart->getItems();
         $this->assertTrue($cartPriceRule->isCartItemsValid($cartItems));
     }
 
-    public function testCartPriceRuleTag()
+    public function testIsCartItemsValidWithMultipleItems()
     {
-        $tagShirt = new Tag();
-        $tagShirt->setId(1);
-        $tagShirt->setName('Navy Shirt');
+        $product1 = new Product;
+        $product1->setid(1);
 
-        $tagPoster = new Tag();
-        $tagPoster->setId(2);
-        $tagPoster->setName('Poster');
-
-        $productShirt = new Product;
-        $productShirt->setSku('TS-NAVY-LG');
-        $productShirt->setName('Navy T-shirt (large)');
-        $productShirt->setUnitPrice(1200);
-        $productShirt->addTag($tagShirt);
-
-        $productPoster = new Product;
-        $productPoster->setSku('PST-CKN');
-        $productPoster->setName('Citizen Kane (1941) Poster');
-        $productPoster->setUnitPrice(500);
-        $productPoster->addTag($tagPoster);
+        $product2 = new Product;
+        $product2->setid(2);
 
         $cartPriceRule = new CartPriceRule;
-        $cartPriceRule->setName('Buy two Shirts get a FREE poster');
-        $cartPriceRule->setStart(new \DateTime('2014-01-01', new \DateTimeZone('UTC')));
-        $cartPriceRule->setEnd(new \DateTime('2014-12-31', new \DateTimeZone('UTC')));
-        $cartPriceRule->addItem(new CartPriceRule\Tag($tagShirt, 2));
-        $cartPriceRule->addItem(new CartPriceRule\Tag($tagPoster, 1));
-        $cartPriceRule->addDiscount(new CartPriceRuleDiscount($productPoster, 1));
+        $cartPriceRule->addItem(new CartPriceRule\Product($product1, 1));
+        $cartPriceRule->addItem(new CartPriceRule\Product($product2, 1));
 
-        $pricing = new Service\Pricing;
+        $cartItems = [
+            new CartItem($product1, 1),
+            new CartItem($product2, 1),
+        ];
 
-        $cart = new Cart;
-        $cartItems = $cart->getItems();
-        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
-
-        $cart = new Cart($pricing);
-        $cart->addItem($productShirt, 1);
-        $cartItems = $cart->getItems();
-        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
-
-        $cart = new Cart($pricing);
-        $cart->addItem($productShirt, 1);
-        $cart->addItem($productPoster, 1);
-        $cartItems = $cart->getItems();
-        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
-
-        $cart = new Cart($pricing);
-        $cart->addItem($productShirt, 2);
-        $cart->addItem($productPoster, 1);
-        $cartItems = $cart->getItems();
         $this->assertTrue($cartPriceRule->isCartItemsValid($cartItems));
+    }
+
+    public function testIsCartItemsValidReturnFalse()
+    {
+        $product1 = new Product;
+        $product1->setid(1);
+
+        $product2 = new Product;
+        $product2->setid(2);
+
+        $cartPriceRule = new CartPriceRule;
+        $cartPriceRule->addItem(new CartPriceRule\Product($product1, 1));
+
+        $cartItems = [new CartItem($product2, 1)];
+
+        $this->assertFalse($cartPriceRule->isCartItemsValid($cartItems));
+    }
+
+    public function testIsValid()
+    {
+        $product = new Product;
+        $product->setid(1);
+
+        $cartPriceRule = new CartPriceRule;
+        $cartPriceRule->addItem(new CartPriceRule\Product($product, 1));
+
+        $cartItems = [new CartItem($product, 1)];
+
+        $this->assertTrue($cartPriceRule->isValid(new \DateTime, $cartItems));
     }
 }
