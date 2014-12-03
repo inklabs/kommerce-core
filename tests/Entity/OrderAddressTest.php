78dddd3be3924f0817cc402aa39cc8805a15e2a3
Jamie Isaacs <pdt256@gmail.com>
2014-12-02 20:33:45 -0800

    Reorganize for 100% for OrderAddress.

---
+++ tests/Entity/OrderAddressTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace inklabs\kommerce\Entity;
+
+class OrderAddressTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $orderAddress = new OrderAddress;
+        $orderAddress->firstName = 'John';
+        $orderAddress->lastName = 'Doe';
+        $orderAddress->company = 'Acme Co.';
+        $orderAddress->address1 = '123 Any St';
+        $orderAddress->address2 = 'Ste 3';
+        $orderAddress->city = 'Santa Monica';
+        $orderAddress->state = 'CA';
+        $orderAddress->zip5 = '90401';
+        $orderAddress->zip4 = '3274';
+        $orderAddress->phone = '555-123-4567';
+        $orderAddress->email = 'john@example.com';
+
+        $this->assertInstanceOf('inklabs\kommerce\Entity\OrderAddress', $orderAddress);
+    }
+}
