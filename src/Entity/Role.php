e2db56dde26753b7725b09e34b000ee7ee5bd255
Jamie Isaacs <pdt256@gmail.com>
2014-12-02 21:32:37 -0800

    Reorganize for 100% for Role.

--- src/Entity/Role.php
+++ src/Entity/Role.php
@@ -14,6 +14,11 @@ class Role
         $this->setCreated();
     }
 
+    public function setId($id)
+    {
+        $this->id = (int) $id;
+    }
+
     public function getId()
     {
         return $this->id;
