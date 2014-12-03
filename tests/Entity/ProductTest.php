fa19f320ef2bf5f42df17d1522e61ef73277b672
Jamie Isaacs <pdt256@gmail.com>
2014-12-02 21:02:39 -0800

    Reorganize for 100% for Product.

--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -5,68 +5,78 @@ use inklabs\kommerce\Service as Service;
 
 class ProductTest extends \PHPUnit_Framework_TestCase
 {
-    public function setUp()
+    public function testCreate()
     {
-        $this->product = new Product;
-        $this->product->setSku('TST101');
-        $this->product->setName('Test Product');
-        $this->product->setUnitPrice(500);
-        $this->product->setQuantity(10);
-        $this->product->setIsInventoryRequired(true);
-        $this->product->setIsPriceVisible(true);
-        $this->product->setIsActive(true);
-        $this->product->setIsVisible(true);
-        $this->product->setIsTaxable(true);
-        $this->product->setIsShippable(true);
-        $this->product->setShippingWeight(16);
-        $this->product->setDescription('Test product description');
-        $this->product->setRating(null);
-        $this->product->setDefaultImage(null);
+        $product = new Product;
+        $product->setId(1);
+        $product->setSku('TST101');
+        $product->setName('Test Product');
+        $product->setUnitPrice(500);
+        $product->setQuantity(10);
+        $product->setIsInventoryRequired(true);
+        $product->setIsPriceVisible(true);
+        $product->setIsActive(true);
+        $product->setIsVisible(true);
+        $product->setIsTaxable(true);
+        $product->setIsShippable(true);
+        $product->setShippingWeight(16);
+        $product->setDescription('Test description');
+        $product->setRating(500);
+        $product->setDefaultImage('http://lorempixel.com/400/200/');
+        $product->addTag(new Tag);
+        $product->addImage(new Image);
+        $product->addProductQuantityDiscount(new ProductQuantityDiscount);
 
-        $this->tag = new Tag;
-        $this->tag->setName('Test Tag');
-        $this->product->addTag($this->tag);
-
-        $this->image = new Image;
-        $this->image->setPath('http://lorempixel.com/400/200/');
-        $this->image->setWidth(400);
-        $this->image->setHeight(200);
-        $this->image->setSortOrder(0);
-        $this->product->addImage($this->image);
+        $this->assertEquals(1, $product->getId());
+        $this->assertEquals('TST101', $product->getSku());
+        $this->assertEquals('Test Product', $product->getName());
+        $this->assertEquals(500, $product->getUnitPrice());
+        $this->assertEquals(10, $product->getQuantity());
+        $this->assertEquals(true, $product->getIsInventoryRequired());
+        $this->assertEquals(true, $product->getIsPriceVisible());
+        $this->assertEquals(true, $product->getIsActive());
+        $this->assertEquals(true, $product->getIsVisible());
+        $this->assertEquals(true, $product->getIsTaxable());
+        $this->assertEquals(true, $product->getIsShippable());
+        $this->assertEquals(16, $product->getShippingWeight());
+        $this->assertEquals('Test description', $product->getDescription());
+        $this->assertEquals(5, $product->getRating());
+        $this->assertEquals('http://lorempixel.com/400/200/', $product->getDefaultImage());
+        $this->assertInstanceOf('inklabs\kommerce\Entity\Tag', $product->getTags()[0]);
+        $this->assertInstanceOf('inklabs\kommerce\Entity\Image', $product->getImages()[0]);
+        $this->assertInstanceOf(
+            'inklabs\kommerce\Entity\ProductQuantityDiscount',
+            $product->getProductQuantityDiscounts()[0]
+        );
     }
 
-    public function testRequiredInStock()
+    public function testInStock()
     {
-        $this->product->setIsInventoryRequired(true);
-
-        $this->product->setQuantity(0);
-        $this->assertFalse($this->product->inStock());
-
-        $this->product->setQuantity(1);
-        $this->assertTrue($this->product->inStock());
+        $product = new Product;
+        $product->setIsInventoryRequired(true);
+        $product->setQuantity(5);
+        $this->assertTrue($product->inStock());
     }
 
-    public function testNotRequiredInStock()
+    public function testInStockWithoutInventoryRequired()
     {
-        $this->product->setIsInventoryRequired(false);
-
-        $this->assertTrue($this->product->inStock());
+        $product = new Product;
+        $product->setIsInventoryRequired(false);
+        $this->assertTrue($product->inStock());
     }
 
-    public function testGetRating()
+    public function testInStockReturnsFalseWhenLackingQuantity()
     {
-        $this->product->setRating(150);
-        $this->assertSame(1.5, $this->product->getRating());
-
-        $this->product->setRating(500);
-        $this->assertSame(5, $this->product->getRating());
+        $product = new Product;
+        $product->setIsInventoryRequired(true);
+        $product->setQuantity(0);
+        $this->assertFalse($product->inStock());
     }
 
-    public function testaddProductQuantityDiscount()
+    public function testGetPrice()
     {
-        $this->product = new Product;
-        $this->product->addProductQuantityDiscount(new ProductQuantityDiscount);
-
-        $this->assertEquals(1, count($this->product->getProductQuantityDiscounts()));
+        $product = new Product;
+        $product->setQuantity(1);
+        $this->assertInstanceOf('inklabs\kommerce\Entity\Price', $product->getPrice(new Service\Pricing));
     }
 }
