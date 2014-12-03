e6e4a365573b5f4b62ada8dd1367b989299b3834
Jamie Isaacs <pdt256@gmail.com>
2014-12-02 21:32:37 -0800

    Reorganize for 100% for Role.

--- tests/Entity/RoleTest.php
+++ tests/Entity/RoleTest.php
@@ -3,17 +3,15 @@ namespace inklabs\kommerce\Entity;
 
 class RoleTest extends \PHPUnit_Framework_TestCase
 {
-    public function setUp()
+    public function testCreate()
     {
-        $this->role = new Role;
-        $this->role->setName('admin');
-        $this->role->setDescription('Administrative user, has access to everything');
-    }
+        $role = new Role;
+        $role->setid(1);
+        $role->setName('admin');
+        $role->setDescription('Test Description');
 
-    public function testGetters()
-    {
-        $this->assertEquals(null, $this->role->getId());
-        $this->assertEquals('admin', $this->role->getName());
-        $this->assertEquals('Administrative user, has access to everything', $this->role->getDescription());
+        $this->assertEquals(1, $role->getId());
+        $this->assertEquals('admin', $role->getName());
+        $this->assertEquals('Test Description', $role->getDescription());
     }
 }
