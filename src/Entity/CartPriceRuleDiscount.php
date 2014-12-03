f74e176f9ff3e436907efc1ce5e11fc0982da652
Jamie Isaacs <pdt256@gmail.com>
2014-12-02 17:52:12 -0800

    Reorganize for 100% for CartPriceRuleDiscount.

--- src/Entity/CartPriceRuleDiscount.php
+++ src/Entity/CartPriceRuleDiscount.php
@@ -6,14 +6,21 @@ class CartPriceRuleDiscount
     use Accessor\Time;
 
     protected $id;
-    protected $product;
     protected $quantity;
+
+    /* @var Product */
+    protected $product;
 
     public function __construct(Product $product = null, $quantity = null)
     {
         $this->setCreated();
         $this->product = $product;
         $this->quantity = $quantity;
+    }
+
+    public function setId($id)
+    {
+        $this->id = (int) $id;
     }
 
     public function getId()
@@ -26,6 +33,9 @@ class CartPriceRuleDiscount
         $this->product = $product;
     }
 
+    /**
+     * @return Product
+     */
     public function getProduct()
     {
         return $this->product;
