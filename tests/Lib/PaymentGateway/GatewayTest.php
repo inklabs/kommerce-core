93e1a6699c4c6761236a2af7be32fa344ab6a164
Jamie Isaacs <pdt256@gmail.com>
2014-12-07 16:00:39 -0800

    Removing shipping dependency.

--- tests/Lib/PaymentGateway/GatewayTest.php
+++ tests/Lib/PaymentGateway/GatewayTest.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pdt256
- * Date: 12/3/14
- * Time: 12:29 AM
- */
-
 namespace inklabs\kommerce\Lib\PaymentGateway;
 
 class GatewayTest extends \PHPUnit_Framework_TestCase
