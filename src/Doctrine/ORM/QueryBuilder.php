c365693107da7c4046228330721b683cd2d231ee
Jamie Isaacs <pdt256@gmail.com>
2014-12-16 01:11:43 -0800

    Adding distance calculations to retrieve warehouses within range.

--- src/Doctrine/ORM/QueryBuilder.php
+++ src/Doctrine/ORM/QueryBuilder.php
@@ -2,11 +2,11 @@
 namespace inklabs\kommerce\Doctrine\ORM;
 
 use Doctrine\ORM\Tools\Pagination\Paginator;
-use inklabs\kommerce\Entity\Pagination;
+use inklabs\kommerce\Entity as Entity;
 
 class QueryBuilder extends \Doctrine\ORM\QueryBuilder
 {
-    public function paginate(Pagination & $pagination = null)
+    public function paginate(Entity\Pagination & $pagination = null)
     {
         if ($pagination === null) {
             return $this;
@@ -41,6 +41,30 @@ class QueryBuilder extends \Doctrine\ORM\QueryBuilder
             ) OR (
                 product.isInventoryRequired = false
             )');
+    }
+
+    public function withinRange(Entity\Point $point, $rangeInMiles = 50, $prefix = '')
+    {
+        $points = $point->getGeoBox($rangeInMiles);
+        $upperLeft = $points[0];
+        $bottomRight = $points[1];
+
+        return $this
+            ->where($prefix . 'latitude BETWEEN :upperLeftLatitude AND :bottomRightLatitude')
+            ->andWhere($prefix . 'longitude BETWEEN :upperLeftLongitude AND :bottomRightLongitude')
+            ->setParameter('upperLeftLatitude', $upperLeft->getLatitude())
+            ->setParameter('upperLeftLongitude', $upperLeft->getLongitude())
+            ->setParameter('bottomRightLatitude', $bottomRight->getLatitude())
+            ->setParameter('bottomRightLongitude', $bottomRight->getLongitude());
+    }
+
+    public function withDistance(Entity\Point $point)
+    {
+        return $this
+            ->addSelect(
+                'DISTANCE(warehouse.address.latitude,warehouse.address.longitude,' .
+                $point->getLatitude() . ',' . $point->getLongitude() . ')'
+            );
     }
 
     public function findAll()
--- src/EntityRepository/Warehouse.php
+++ src/EntityRepository/Warehouse.php
@@ -6,4 +6,21 @@ use inklabs\kommerce\Entity as Entity;
 
 class Warehouse extends EntityRepository
 {
+    /**
+     * @return Entity\Warehouse[]
+     */
+    public function findByPoint(Entity\Point $point, $rangeInMiles = 50, Entity\Pagination & $pagination = null)
+    {
+        $qb = $this->getQueryBuilder();
+
+        $warehouses = $qb
+            ->select('warehouse')
+            ->from('kommerce:Warehouse', 'warehouse')
+            ->withDistance($point)
+            ->withinRange($point, $rangeInMiles, 'warehouse.address.')
+            ->paginate($pagination)
+            ->findAll();
+
+        return $warehouses;
+    }
 }
