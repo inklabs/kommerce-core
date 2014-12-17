6677ffde6fa6669c1964d53bc359b82201c6a89a
Jamie Isaacs <pdt256@gmail.com>
2014-12-16 17:55:10 -0800

    Adding base repository classes.

---
+++ src/EntityRepository/CartPriceRuleDiscount.php
@@ -0,0 +1,9 @@
+<?php
+namespace inklabs\kommerce\EntityRepository;
+
+use inklabs\kommerce\Doctrine\ORM\EntityRepository;
+use inklabs\kommerce\Entity as Entity;
+
+class CartPriceRuleDiscount extends EntityRepository
+{
+}
