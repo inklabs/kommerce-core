423d69dc721096687b4cc61eef34b1a71a95046f
Jamie Isaacs <pdt256@gmail.com>
2015-01-04 21:27:52 -0800

    Removing comments.

--- src/Entity/CartTotal.php
+++ src/Entity/CartTotal.php
@@ -3,12 +3,12 @@ namespace inklabs\kommerce\Entity;
 
 class CartTotal
 {
-    public $origSubtotal = 0;      // Used to calculate total savings
-    public $subtotal = 0;          // Subtotal after catalog promotions
-    public $taxSubtotal = 0;       // Total used for tax calculation
-    public $discount = 0;          // Coupons and cart price rules
+    public $origSubtotal = 0;
+    public $subtotal = 0;
+    public $taxSubtotal = 0;
+    public $discount = 0;
     public $shipping = 0;
-    public $shippingDiscount = 0; // Coupons and cart price rules (separate from $discount)
+    public $shippingDiscount = 0;
     public $tax = 0;
     public $total = 0;
     public $savings = 0;
