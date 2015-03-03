bb3dd04bdbf24b19179a3701a7ee86d46b945127
Jamie Isaacs <pdt256@gmail.com>
2015-03-02 21:25:22 -0800

    Adding tag service function to retrieve all tags.

--- src/EntityRepository/Tag.php
+++ src/EntityRepository/Tag.php
@@ -6,4 +6,27 @@ use inklabs\kommerce\Entity as Entity;
 
 class Tag extends EntityRepository
 {
+    /**
+     * @return Entity\View\Tag[]
+     */
+    public function getAllTags($queryString = null, Entity\Pagination & $pagination = null)
+    {
+        $qb = $this->getQueryBuilder();
+
+        $tags = $qb->select('tag')
+            ->from('kommerce:tag', 'tag');
+
+        if ($queryString !== null) {
+            $tags = $tags
+                ->where('tag.name LIKE :query')
+                ->setParameter('query', '%' . $queryString . '%');
+        }
+
+        $tags = $tags
+            ->paginate($pagination)
+            ->getQuery()
+            ->getResult();
+
+        return $tags;
+    }
 }
--- tests/EntityRepository/TagTest.php
+++ tests/EntityRepository/TagTest.php
@@ -32,11 +32,39 @@ class TagTest extends Helper\DoctrineTestCase
         $this->entityManager->clear();
     }
 
+    /**
+     * @return Entity\Tag
+     */
+    private function getDummyTag($num)
+    {
+        $tag = new Entity\Tag;
+        $tag->setName('Test Tag ' . $num);
+        $tag->setDescription('Test Description');
+        $tag->setDefaultImage('http://lorempixel.com/400/200/');
+        $tag->setSortOrder(0);
+        $tag->setIsVisible(true);
+        return $tag;
+    }
+
     public function testFind()
     {
         $tag = $this->getRepository()
             ->find(1);
 
         $this->assertSame(1, $tag->getId());
+    }
+
+    public function testGetAllTags()
+    {
+        $tag1 = $this->getDummyTag(1);
+
+        $this->entityManager->persist($tag1);
+        $this->entityManager->flush();
+        $this->entityManager->clear();
+
+        $tags = $this->getRepository()
+            ->getAllTags('Test');
+
+        $this->assertSame(1, $tags[0]->getId());
     }
 }
