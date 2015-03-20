7e45d205877b811263e46be13f3d31060fe32f86
Jamie Isaacs <pdt256@gmail.com>
2015-03-20 15:36:17 -0700

    Cleaning up UserToken test.

--- src/EntityRepository/UserToken.php
+++ src/EntityRepository/UserToken.php
@@ -4,6 +4,9 @@ namespace inklabs\kommerce\EntityRepository;
 use inklabs\kommerce\Doctrine\ORM\EntityRepository;
 use inklabs\kommerce\Entity as Entity;
 
+/**
+ * @method Entity\UserToken find($id)
+ */
 class UserToken extends EntityRepository
 {
 }
