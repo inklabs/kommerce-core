dc33648abb43de4a09f0d1edb4475579d813dd9b
Jamie Isaacs <pdt256@gmail.com>
2015-03-20 23:49:48 -0700

    Cleaning up CartPriceRule test.

--- src/EntityRepository/CartPriceRule.php
+++ src/EntityRepository/CartPriceRule.php
@@ -4,6 +4,9 @@ namespace inklabs\kommerce\EntityRepository;
 use inklabs\kommerce\Doctrine\ORM\EntityRepository;
 use inklabs\kommerce\Entity as Entity;
 
+/**
+ * @method Entity\CartPriceRule find($id)
+ */
 class CartPriceRule extends EntityRepository
 {
 }
--- src/EntityRepository/CartPriceRuleDiscount.php
+++ src/EntityRepository/CartPriceRuleDiscount.php
@@ -4,6 +4,9 @@ namespace inklabs\kommerce\EntityRepository;
 use inklabs\kommerce\Doctrine\ORM\EntityRepository;
 use inklabs\kommerce\Entity as Entity;
 
+/**
+ * @method Entity\CartPriceRuleDiscount find($id)
+ */
 class CartPriceRuleDiscount extends EntityRepository
 {
 }
