bb4d2cf30ebcaed4ebc97a2b19c17aa147d6c8d1
Jamie Isaacs <pdt256@gmail.com>
2015-03-21 00:05:24 -0700

    Adding test for CartPriceRuleItem.

---
+++ src/EntityRepository/CartPriceRuleItem/Item.php
@@ -0,0 +1,12 @@
+<?php
+namespace inklabs\kommerce\EntityRepository\CartPriceRuleItem;
+
+use inklabs\kommerce\Doctrine\ORM\EntityRepository;
+use inklabs\kommerce\Entity as Entity;
+
+/**
+ * @method Entity\CartPriceRuleItem\Product|Entity\CartPriceRuleItem\Tag find($id)
+ */
+class Item extends EntityRepository
+{
+}
