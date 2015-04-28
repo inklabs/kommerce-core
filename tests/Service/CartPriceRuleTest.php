33c0569bd4f48c792772e1f5682fdf3ad34c7d20
Jamie Isaacs <pdt256@gmail.com>
2015-04-27 23:33:53 -0700

    Adding Service coverage.

--- tests/Service/CartPriceRuleTest.php
+++ tests/Service/CartPriceRuleTest.php
@@ -19,6 +19,20 @@ class CartPriceRuleTest extends Helper\DoctrineTestCase
         $this->cartPriceRuleService = new CartPriceRule($this->cartPriceRuleRepository);
     }
 
+    public function testFind()
+    {
+        $product = $this->cartPriceRuleService->find(1);
+        $this->assertTrue($product instanceof Entity\CartPriceRule);
+    }
+
+    public function testFindMissing()
+    {
+        $this->cartPriceRuleRepository->setReturnValue(null);
+
+        $product = $this->cartPriceRuleService->find(1);
+        $this->assertSame(null, $product);
+    }
+
     public function testFindAll()
     {
         $cartPriceRules = $this->cartPriceRuleService->findAll();
