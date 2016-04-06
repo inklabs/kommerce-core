e65b1613839b71d0cd1fcdd707a42d49a3304693
Jamie Isaacs <pdt256@gmail.com>
2016-04-06 15:12:30 -0700

    Cleaning up price test.

--- tests/Entity/PriceTest.php
+++ tests/Entity/PriceTest.php
@@ -5,19 +5,38 @@ use inklabs\kommerce\tests\Helper\DoctrineTestCase;
 
 class PriceTest extends DoctrineTestCase
 {
+    public function testCreateDefaults()
+    {
+        $price = new Price;
+
+        $this->assertSame(null, $price->origQuantityPrice);
+        $this->assertSame(null, $price->unitPrice);
+        $this->assertSame(null, $price->origQuantityPrice);
+        $this->assertSame(null, $price->quantityPrice);
+        $this->assertSame(0, count($price->getCatalogPromotions()));
+        $this->assertSame(0, count($price->getProductQuantityDiscounts()));
+    }
+
     public function testCreate()
     {
+        $catalogPromotion = $this->dummyData->getCatalogPromotion();
+        $productQuantityDiscount = $this->dummyData->getProductQuantityDiscount();
+
         $price = new Price;
         $price->origUnitPrice = 2500;
         $price->unitPrice = 1750;
         $price->origQuantityPrice = 2500;
         $price->quantityPrice = 1750;
-        $price->addCatalogPromotion(new CatalogPromotion);
-        $price->addProductQuantityDiscount(new ProductQuantityDiscount);
+        $price->addCatalogPromotion($catalogPromotion);
+        $price->addProductQuantityDiscount($productQuantityDiscount);
 
         $this->assertEntityValid($price);
-        $this->assertTrue($price->getCatalogPromotions()[0] instanceof CatalogPromotion);
-        $this->assertTrue($price->getProductQuantityDiscounts()[0] instanceof ProductQuantityDiscount);
+        $this->assertSame(2500, $price->origUnitPrice);
+        $this->assertSame(1750, $price->unitPrice);
+        $this->assertSame(2500, $price->origQuantityPrice);
+        $this->assertSame(1750, $price->quantityPrice);
+        $this->assertSame($catalogPromotion, $price->getCatalogPromotions()[0]);
+        $this->assertSame($productQuantityDiscount, $price->getProductQuantityDiscounts()[0]);
     }
 
     public function testAdd()
@@ -43,20 +62,20 @@ class PriceTest extends DoctrineTestCase
         $two->addCatalogPromotion($catalogPromotion2);
         $two->addProductQuantityDiscount($productQuantityDiscount2);
 
-        $three = new Price;
-        $three->unitPrice         = 3;
-        $three->origUnitPrice     = 3;
-        $three->quantityPrice     = 3;
-        $three->origQuantityPrice = 3;
-        $three->addCatalogPromotion($catalogPromotion1);
-        $three->addCatalogPromotion($catalogPromotion2);
-        $three->addProductQuantityDiscount($productQuantityDiscount1);
-        $three->addProductQuantityDiscount($productQuantityDiscount2);
+        $expectedPrice = new Price;
+        $expectedPrice->unitPrice         = 3;
+        $expectedPrice->origUnitPrice     = 3;
+        $expectedPrice->quantityPrice     = 3;
+        $expectedPrice->origQuantityPrice = 3;
+        $expectedPrice->addCatalogPromotion($catalogPromotion1);
+        $expectedPrice->addCatalogPromotion($catalogPromotion2);
+        $expectedPrice->addProductQuantityDiscount($productQuantityDiscount1);
+        $expectedPrice->addProductQuantityDiscount($productQuantityDiscount2);
 
-        $this->assertEquals($three, Price::add($one, $two));
+        $this->assertEquals($expectedPrice, Price::add($one, $two));
     }
 
-    public function testAddAndPreventDuplicates()
+    public function testAddPreventsDuplicates()
     {
         $catalogPromotion = $this->dummyData->getCatalogPromotion();
         $productQuantityDiscount = $this->dummyData->getProductQuantityDiscount();
