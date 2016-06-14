6ec8c812c02cd607dcf5a30282b5f71f06280a0e
Jamie Isaacs <pdt256@gmail.com>
2016-06-13 21:16:11 -0700

    Adding methods to QueryInterface.

--- src/Lib/Query/QueryInterface.php
+++ src/Lib/Query/QueryInterface.php
@@ -3,4 +3,13 @@ namespace inklabs\kommerce\Lib\Query;
 
 interface QueryInterface
 {
+    /**
+     * @return mixed
+     */
+    public function getRequest();
+
+    /**
+     * @return mixed
+     */
+    public function getResponse();
 }
--- tests/Helper/Action/FakeQuery.php
+++ tests/Helper/Action/FakeQuery.php
@@ -7,7 +7,25 @@ use inklabs\kommerce\tests\Helper\Action\Query\FakeResponseInterface;
 
 final class FakeQuery implements QueryInterface
 {
+    /** @var FakeRequest */
+    private $request;
+
+    /** @var FakeResponseInterface */
+    private $response;
+
     public function __construct(FakeRequest $request, FakeResponseInterface & $response)
     {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
     }
 }
