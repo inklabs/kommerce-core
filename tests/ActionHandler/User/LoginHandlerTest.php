9e4df35c350302355a5206677647f81fc221ff70
Jamie Isaacs <pdt256@gmail.com>
2017-01-26 19:56:36 -0800

    Refactoring LoginCommand test. https://github.com/inklabs/kommerce-core/issues/96

--- tests/ActionHandler/User/LoginHandlerTest.php
+++ tests/ActionHandler/User/LoginHandlerTest.php
@@ -2,25 +2,44 @@
 namespace inklabs\kommerce\ActionHandler\User;
 
 use inklabs\kommerce\Action\User\LoginCommand;
+use inklabs\kommerce\Entity\Cart;
+use inklabs\kommerce\Entity\TaxRate;
+use inklabs\kommerce\Entity\User;
+use inklabs\kommerce\Entity\UserLogin;
+use inklabs\kommerce\Entity\UserRole;
+use inklabs\kommerce\Entity\UserToken;
 use inklabs\kommerce\tests\Helper\TestCase\ActionTestCase;
 
 class LoginHandlerTest extends ActionTestCase
 {
+    protected $metaDataClassNames = [
+        User::class,
+        UserLogin::class,
+        UserRole::class,
+        UserToken::class,
+        Cart::class,
+        TaxRate::class,
+    ];
+
     public function testHandle()
     {
+        $password = 'password1';
         $user = $this->dummyData->getUser();
-
-        $userService = $this->mockService->getUserService();
-        $userService->shouldReceive('login')
-            ->once();
+        $user->setPassword($password);
+        $this->persistEntityAndFlushClear($user);
 
         $command = new LoginCommand(
             $user->getEmail(),
-            'password1',
+            $password,
             self::IP4
         );
+        $this->dispatchCommand($command);
 
-        $handler = new LoginHandler($userService);
-        $handler->handle($command);
+        $this->entityManager->clear();
+        $user = $this->getRepositoryFactory()->getUserRepository()->findOneById(
+            $user->getId()
+        );
+        $login = $user->getUserLogins()[0];
+        $this->assertSame(self::IP4, $login->getIp4());
     }
 }
