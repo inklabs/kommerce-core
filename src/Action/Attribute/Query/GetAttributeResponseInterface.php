90531b294a289fbd7f64fd25df81a525ffa6e066
Jamie Isaacs <pdt256@gmail.com>
2017-03-17 23:09:28 -0700

    Refactoring GetAttributeQuery. #88

--- src/Action/Attribute/Query/GetAttributeResponseInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace inklabs\kommerce\Action\Attribute\Query;
-
-use inklabs\kommerce\EntityDTO\Builder\AttributeDTOBuilder;
-
-interface GetAttributeResponseInterface
-{
-    public function setAttributeDTOBuilder(AttributeDTOBuilder $attributeDTOBuilder);
-}
