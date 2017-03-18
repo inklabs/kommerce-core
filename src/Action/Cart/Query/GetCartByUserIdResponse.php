94926717c881a35bfc57b6d375540202d9e0a337
Jamie Isaacs <pdt256@gmail.com>
2017-03-17 23:43:43 -0700

    Refactoring GetCartByUserIdQuery. #88

--- src/Action/Cart/Query/GetCartByUserIdResponse.php
+++ src/Action/Cart/Query/GetCartByUserIdResponse.php
@@ -2,20 +2,11 @@
 namespace inklabs\kommerce\Action\Cart\Query;
 
 use inklabs\kommerce\EntityDTO\Builder\CartDTOBuilder;
-use inklabs\kommerce\Lib\CartCalculator;
 
 class GetCartByUserIdResponse implements GetCartByUserIdResponseInterface
 {
     /** @var CartDTOBuilder */
     private $cartDTOBuilder;
-
-    /** @var CartCalculator */
-    private $cartCalculator;
-
-    public function __construct(CartCalculator $cartCalculator)
-    {
-        $this->cartCalculator = $cartCalculator;
-    }
 
     public function setCartDTOBuilder(CartDTOBuilder $cartDTOBuilder)
     {
