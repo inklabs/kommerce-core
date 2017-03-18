d2fdc3f34ba915fc49913601c37729b3c77387af
Jamie Isaacs <pdt256@gmail.com>
2017-03-18 12:06:21 -0700

    Refactoring GetCouponQuery. #88

--- src/Action/Coupon/Query/GetCouponResponseInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace inklabs\kommerce\Action\Coupon\Query;
-
-use inklabs\kommerce\EntityDTO\Builder\CouponDTOBuilder;
-
-interface GetCouponResponseInterface
-{
-    public function setCouponDTOBuilder(CouponDTOBuilder $couponDTOBuilder);
-}
