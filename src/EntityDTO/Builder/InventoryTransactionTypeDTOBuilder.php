0ce31bf12ebc2b8d2ee6587633b06358c07a6fc0
Jamie Isaacs <pdt256@gmail.com>
2017-03-22 23:29:44 -0700

    Adding InventoryTransactionType boolean values to DTO.

--- src/EntityDTO/Builder/InventoryTransactionTypeDTOBuilder.php
+++ src/EntityDTO/Builder/InventoryTransactionTypeDTOBuilder.php
@@ -1,6 +1,7 @@
 <?php
 namespace inklabs\kommerce\EntityDTO\Builder;
 
+use inklabs\kommerce\Entity\InventoryTransactionType;
 use inklabs\kommerce\EntityDTO\InventoryTransactionTypeDTO;
 
 /**
@@ -8,8 +9,28 @@ use inklabs\kommerce\EntityDTO\InventoryTransactionTypeDTO;
  */
 class InventoryTransactionTypeDTOBuilder extends AbstractIntegerTypeDTOBuilder
 {
+    /** @var InventoryTransactionType */
+    protected $entity;
+
+    /** @var InventoryTransactionTypeDTO */
+    protected $entityDTO;
+
     protected function getEntityDTO()
     {
         return new InventoryTransactionTypeDTO;
+    }
+
+    public function __construct(InventoryTransactionType $type)
+    {
+        parent::__construct($type);
+
+        $this->entityDTO->isMove = $this->entity->isMove();
+        $this->entityDTO->isHold = $this->entity->isHold();
+        $this->entityDTO->isNewProducts = $this->entity->isNewProducts();
+        $this->entityDTO->isShipped = $this->entity->isShipped();
+        $this->entityDTO->isReturned = $this->entity->isReturned();
+        $this->entityDTO->isPromotion = $this->entity->isPromotion();
+        $this->entityDTO->isDamaged = $this->entity->isDamaged();
+        $this->entityDTO->isShrinkage = $this->entity->isShrinkage();
     }
 }
--- src/EntityDTO/InventoryTransactionTypeDTO.php
+++ src/EntityDTO/InventoryTransactionTypeDTO.php
@@ -3,4 +3,27 @@ namespace inklabs\kommerce\EntityDTO;
 
 class InventoryTransactionTypeDTO extends AbstractIntegerTypeDTO
 {
+    /** @var bool */
+    public $isMove;
+
+    /** @var bool */
+    public $isHold;
+
+    /** @var bool */
+    public $isNewProducts;
+
+    /** @var bool */
+    public $isShipped;
+
+    /** @var bool */
+    public $isReturned;
+
+    /** @var bool */
+    public $isPromotion;
+
+    /** @var bool */
+    public $isDamaged;
+
+    /** @var bool */
+    public $isShrinkage;
 }
